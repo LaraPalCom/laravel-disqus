b530c570b6c2ac87ab52f46d99e199f1fbc08499
Roumen Damianoff <roumen@dawebs.com>
2013-06-02 17:38:13 +0300

    added disqusapi

---
+++ src/Roumen/Disqus/DisqusServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Roumen\Disqus;
+
+use Illuminate\Support\ServiceProvider;
+
+class DisqusServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('roumen/disqus');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
